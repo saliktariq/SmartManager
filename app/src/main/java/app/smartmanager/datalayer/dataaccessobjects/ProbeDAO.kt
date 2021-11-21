b284c6f3a4f12f13c6b2dac1c91f8ad7787c43c7
Salik Tariq <saliktariq@icloud.com>
2021-11-21 10:28:19 +0000

    ProbeDAO
    updated

--- app/src/main/java/app/smartmanager/datalayer/dataaccessobjects/ProbeDAO.kt
+++ app/src/main/java/app/smartmanager/datalayer/dataaccessobjects/ProbeDAO.kt
@@ -1,6 +1,7 @@
 package app.smartmanager.datalayer.dataaccessobjects
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
 import app.smartmanager.datalayer.entity.Probe
@@ -16,19 +17,19 @@ interface ProbeDAO {
     @Query("Select * from probe")
     fun getAllProbeData(): List<Probe>?
 
-    // Query to retrieve data related to a probe based on given probeID
-    @Query("Select * from probe WHERE probeID = :key ORDER BY probeID DESC LIMIT 1")
-    fun getProbeByID(key: Long): Probe?
-
     // Query to retrieve data related to a probe based on given probeName
     @Query("Select * from probe WHERE probeName = :key ORDER BY probeName DESC LIMIT 1")
     fun getProbeByName(key: String): Probe?
 
-    //Query to update probeName based on probeID
-    @Query("UPDATE probe SET probeName = :newProbeName WHERE probeID = :givenProbeID")
-    fun updateProbeName(newProbeName: String, givenProbeID: Long)
+    //Query to update probeName based on probeName
+    @Query("UPDATE probe SET probeName = :newProbeName WHERE probeName = :givenProbeName")
+    fun updateProbeName(newProbeName: String, givenProbeName: String)
 
-    //Query to delete a record based on a given probeID
-    @Query("DELETE from probe WHERE probeID = :givenProbeID")
-    fun deleteProbeByID(givenProbeID: Long)
+    //Query to delete a record based on a given probeName
+    @Query("DELETE from probe WHERE probeName = :givenProbeName")
+    fun deleteProbeByName(givenProbeName: String)
+
+    //Query to delete a record based on a given probe object
+    @Delete
+    fun deleteProbeByProbeObject(probe: Probe)
 }
