2a2de46492357f69c85ea8cacd1694f41577a9c4
Salik Tariq <saliktariq@icloud.com>
2021-11-21 10:32:41 +0000

    Update ProbeViewModel.kt
    ProbeViewModel updated to include recyclerview related functions. Queries optimised for concurrency

--- app/src/main/java/app/smartmanager/ui/initialsetup/ProbeViewModel.kt
+++ app/src/main/java/app/smartmanager/ui/initialsetup/ProbeViewModel.kt
@@ -1,10 +1,13 @@
 package app.smartmanager.ui.initialsetup
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import app.smartmanager.datalayer.entity.Probe
 import app.smartmanager.helper.GetAppContext
 import app.smartmanager.helper.ToastMaker
 import app.smartmanager.repository.SmartManagerRepo
+import kotlinx.coroutines.launch
 
 class ProbeViewModel : ViewModel() {
 
@@ -20,23 +23,54 @@ class ProbeViewModel : ViewModel() {
     //Variables to hold data received from repository
     var retrievedProbeName: String? = null
 
+    //Variable to hold Mutable Live Data related to Probes stored in database
+    var allProbes: MutableLiveData<List<Probe>>
+
+    // Probe data observer
+    fun getAllProbesObserver(): MutableLiveData<List<Probe>> {
+        return allProbes
+    }
+
+    init {
+        // initialising allProbes
+        allProbes = MutableLiveData()
+
+        viewModelScope.launch {
+            getAllProbeData()
+        }
+
+    }
+
     //Function to insert Probe Data
     suspend fun insertProbeData(name: String): Boolean{
+        var probeData: Probe? = null
+
         if(name.length > 1){
-            val probeData = repository.getProbeByName(name)
+            val runQuery = viewModelScope.launch {
+                probeData = repository.getProbeByName(name)
+            }
+            runQuery.join()
+
             if (probeData != null){
-                retrievedProbeName = probeData.probeName
+                retrievedProbeName = probeData!!.probeName
             }
 
             if(retrievedProbeName.equals(probeName))
             {
                 probeNameAlreadyExist = true
 
-                ToastMaker.showToast("Probe name already exists.", GetAppContext.appContext)
+                //    ToastMaker.showToast("Probe name already exists.", GetAppContext.appContext)
                 return false
             } else {
-                val newProbeObject = Probe(0,name)
-                repository.insertProbeData(newProbeObject)
+                val newProbeObject = Probe(name)
+                val insertOperation =  viewModelScope.launch {
+                    repository.insertProbeData(newProbeObject)
+                }
+                insertOperation.join()
+                val runUpdate = viewModelScope.launch {
+                    getAllProbeData()
+                }
+                runUpdate.join()
 
                 ToastMaker.showToast("Probe successfully added.", GetAppContext.appContext)
                 return true
@@ -46,5 +80,52 @@ class ProbeViewModel : ViewModel() {
         ToastMaker.showToast("Error! Enter a valid probe name.", GetAppContext.appContext)
         return false
     }
+
+    // Function to retrieve all Probe Data
+    suspend fun getAllProbeData(){
+        val run = viewModelScope.launch {
+            // Variable to hold all probe data retrieved
+            val list = repository.getAllProbeData()
+            allProbes.postValue(list)
+        }
+        run.join()
+
+    }
+
+
+    // Function to delete a probe dataset by giving probeName
+    suspend fun deleteProbeByName(probeName: String){
+        val runDelete = viewModelScope.launch {
+            repository.deleteProbeByName(probeName)
+
+        }
+        runDelete.join()
+        val runUpdate = viewModelScope.launch {
+            getAllProbeData()
+        }
+        runUpdate.join()
+
+    }
+
+
+    // Function to delete a probe dataset by giving probe object
+    suspend fun deleteProbeByProbeObject(probe: Probe){
+        val runDelete = viewModelScope.launch {
+            repository.deleteProbeByProbeObject(probe)
+
+        }
+        runDelete.join()
+        val runUpdate = viewModelScope.launch {
+            getAllProbeData()
+        }
+        runUpdate.join()
+
+    }
+
+
+
+
+
+
 
 }
