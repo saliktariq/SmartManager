1802b957628c29744adc007b669feac59f61e03e
Salik Tariq <saliktariq@icloud.com>
2021-11-23 01:57:13 +0000

    CleaningTask & ChemicalList DAO
    INSERT and READ ALL functions implemented

--- app/src/main/java/app/smartmanager/datalayer/dataaccessobjects/ChemicalListCOSHHDAO.kt
+++ app/src/main/java/app/smartmanager/datalayer/dataaccessobjects/ChemicalListCOSHHDAO.kt
@@ -1,4 +1,21 @@
 package app.smartmanager.datalayer.dataaccessobjects
 
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy.IGNORE
+import androidx.room.Query
+import app.smartmanager.datalayer.entity.ChemicalListCOSHH
+
+
+@Dao
 interface ChemicalListCOSHHDAO {
+
+    @Insert(onConflict = IGNORE)
+    suspend fun addChemicalListCOSHH(chemicalListCOSHH: ChemicalListCOSHH)
+
+    @Query("SELECT * from chemicalList ORDER BY id ASC")
+    suspend fun readAllChemicalListCOSHHData(): LiveData<List<ChemicalListCOSHH>>
+    
+
 }
--- app/src/main/java/app/smartmanager/datalayer/dataaccessobjects/CleaningTaskDAO.kt
+++ app/src/main/java/app/smartmanager/datalayer/dataaccessobjects/CleaningTaskDAO.kt
@@ -1,4 +1,22 @@
 package app.smartmanager.datalayer.dataaccessobjects
 
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Ignore
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy.IGNORE
+import androidx.room.Query
+import app.smartmanager.datalayer.entity.CleaningTask
+
+
+@Dao
 interface CleaningTaskDAO {
+
+
+    @Insert(onConflict = IGNORE)
+    suspend fun addCleaningTask(cleaningTask: CleaningTask)
+
+    @Query("SELECT * from cleaningTask ORDER BY id ASC")
+    suspend fun readAllCleaningTaskData(): LiveData<List<CleaningTask>>
+
 }
