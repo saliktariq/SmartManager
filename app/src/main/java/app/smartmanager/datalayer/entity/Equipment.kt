0886346ef7eda6168d08373013f5a613a305b133
Salik Tariq <saliktariq@icloud.com>
2021-11-23 01:27:39 +0000

    Create Equipment.kt
    equipment entity class created

---
+++ app/src/main/java/app/smartmanager/datalayer/entity/Equipment.kt
@@ -0,0 +1,12 @@
+package app.smartmanager.datalayer.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "equipment")
+data class Equipment(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long,
+    val name: String,
+    val temperatureControl: Boolean
+)
