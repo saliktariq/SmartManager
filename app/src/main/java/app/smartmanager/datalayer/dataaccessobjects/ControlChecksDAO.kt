d32a9ee06da4408015183ff2c0e98efaecca437f
Salik Tariq <saliktariq@icloud.com>
2021-11-23 01:51:29 +0000

    Update ControlChecksDAO.kt
    INSERT and READ ALL functions added to ControlChecks DAO

--- app/src/main/java/app/smartmanager/datalayer/dataaccessobjects/ControlChecksDAO.kt
+++ app/src/main/java/app/smartmanager/datalayer/dataaccessobjects/ControlChecksDAO.kt
@@ -1,4 +1,19 @@
 package app.smartmanager.datalayer.dataaccessobjects
 
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import app.smartmanager.datalayer.entity.ControlChecks
+
+
+@Dao
 interface ControlChecksDAO {
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun addControlChecks(controlChecks: ControlChecks)
+
+    @Query("SELECT * from controlChecks ORDER BY id ASC")
+    fun readAllControlChecksData(): LiveData<List<ControlChecks>>
 }
