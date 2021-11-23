56c611930d7bf546663f1da40b729e12d5e132cf
Salik Tariq <saliktariq@icloud.com>
2021-11-23 01:45:30 +0000

    Data access object file structure added

---
+++ app/src/main/java/app/smartmanager/datalayer/dataaccessobjects/EquipmentDAO.kt
@@ -0,0 +1,4 @@
+package app.smartmanager.datalayer.dataaccessobjects
+
+interface EquipmentDAO {
+}
--- app/src/main/java/app/smartmanager/datalayer/dataaccessobjects/SupplierDAO.kt
+++ app/src/main/java/app/smartmanager/datalayer/dataaccessobjects/SupplierDAO.kt
@@ -1,5 +1,21 @@
 package app.smartmanager.datalayer.dataaccessobjects
 
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy.IGNORE
+import androidx.room.Query
+import app.smartmanager.datalayer.entity.Supplier
+
+@Dao
 interface SupplierDAO {
+
+    @Insert(onConflict = IGNORE)
+    suspend fun addSupplier(supplier: Supplier)
+
+    @Query("SELECT * from supplier ORDER BY id ASC")
+    fun readAllSupplierData(): LiveData<List<Supplier>>
+
+    
 
 }
