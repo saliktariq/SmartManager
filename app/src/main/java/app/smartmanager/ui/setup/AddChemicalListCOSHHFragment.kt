f38b288d8389d1909f3ae8e8c9e0254eff33d089
Salik Tariq <saliktariq@icloud.com>
2021-11-23 07:40:45 +0000

    New Fragment Classes cleaned
    removed default code from 'add' fragments

---
+++ app/src/main/java/app/smartmanager/ui/setup/AddChemicalListCOSHHFragment.kt
@@ -0,0 +1,23 @@
+package app.smartmanager.ui.setup
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import app.smartmanager.R
+
+
+class AddChemicalListCOSHHFragment : Fragment() {
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_chemical_list_coshh, container, false)
+    }
+
+}
---
+++ app/src/main/java/app/smartmanager/ui/setup/AddCleaningTaskFragment.kt
@@ -0,0 +1,21 @@
+package app.smartmanager.ui.setup
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import app.smartmanager.R
+
+class AddCleaningTaskFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_cleaning_task, container, false)
+    }
+
+}
