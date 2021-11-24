0ec76dc69ce43cc44dcd96a1feb4d5471ba142d3
Salik Tariq <saliktariq@icloud.com>
2021-11-24 10:51:00 +0000

    Updated gradle
    updated gradle to include parcelize plugin

--- app/src/main/java/app/smartmanager/datalayer/entity/Supplier.kt
+++ app/src/main/java/app/smartmanager/datalayer/entity/Supplier.kt
@@ -1,7 +1,9 @@
 package app.smartmanager.datalayer.entity
 
+import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+
 
 @Entity(tableName = "supplier")
 data class Supplier(
