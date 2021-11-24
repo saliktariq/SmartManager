9497c038ffcade7f2ee83cd1782f5d7519d97ec9
Salik Tariq <saliktariq@icloud.com>
2021-11-24 07:02:32 +0000

    Updated Action Bar
    updated action bar to include name of fragment and back button

--- app/src/main/java/app/smartmanager/MainActivity.kt
+++ app/src/main/java/app/smartmanager/MainActivity.kt
@@ -2,10 +2,20 @@ package app.smartmanager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /*
+        Following functionality implemented following https://stackoverflow.com/questions/50502269/illegalstateexception-link-does-not-have-a-navcontroller-set
+         */
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        setupActionBarWithNavController(navController)
     }
 }
--- app/src/main/java/app/smartmanager/ui/setup/SupplierFragment.kt
+++ app/src/main/java/app/smartmanager/ui/setup/SupplierFragment.kt
@@ -1,33 +1,35 @@
 package app.smartmanager.ui.setup
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import app.smartmanager.R
-import app.smartmanager.ui.setup.viewmodel.SupplierViewModel
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class SupplierFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = SupplierFragment()
-    }
 
-    private lateinit var viewModel: SupplierViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.supplier_fragment, container, false)
-    }
+        val fragmentView = inflater.inflate(R.layout.supplier_fragment, container, false)
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(SupplierViewModel::class.java)
-        // TODO: Use the ViewModel
+        val floatingAddButton = fragmentView.findViewById<FloatingActionButton>(R.id.btnAdd)
+
+        // Attaching onClickListener to Add button (floatingAddButton)
+        floatingAddButton.setOnClickListener {
+            findNavController().navigate(R.id.action_supplierFragment_to_addSupplierFragment)
+
+        }
+
+        return fragmentView
+
     }
 
 }
+
