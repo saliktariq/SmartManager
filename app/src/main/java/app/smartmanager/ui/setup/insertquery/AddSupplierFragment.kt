28003e13df37d7e2c925e5a342af477ca1bc73c3
Salik Tariq <saliktariq@icloud.com>
2021-11-24 07:53:41 +0000

    Supplier Insert Data
    insert data function for supplier object implemented

--- app/src/main/java/app/smartmanager/ui/setup/insertquery/AddSupplierFragment.kt
+++ app/src/main/java/app/smartmanager/ui/setup/insertquery/AddSupplierFragment.kt
@@ -5,15 +5,67 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import app.smartmanager.R
+import app.smartmanager.ui.setup.viewmodel.SupplierViewModel
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+//import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class AddSupplierFragment : Fragment() {
+
+    private lateinit var supplierViewModel: SupplierViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_supplier, container, false)
+
+        val fragmentView = inflater.inflate(R.layout.fragment_add_supplier, container, false)
+
+        // Initialising the supplierViewModel
+
+        supplierViewModel = ViewModelProvider(this).get(SupplierViewModel::class.java)
+
+     //   val addButton = fragmentView.findViewById<FloatingActionButton>(R.id.btnAdd)
+
+        val addButton: Button = fragmentView.findViewById(R.id.btnAdd)
+
+
+
+        // Variables to hold fragment data
+        lateinit var supplierName: String //This property can not be null as per our entity definition
+        var supplierEmail: String? = null
+        var supplierPhone: String? = null
+        var supplierAddress: String? = null
+
+
+
+// Attaching onClickListener to Add button (addButton)
+        addButton.setOnClickListener {
+
+            //Initialising fragment data variables with user entered data
+            supplierName = fragmentView.findViewById<EditText>(R.id.supplierName).text.toString()
+            supplierEmail = fragmentView.findViewById<EditText>(R.id.supplierEmail).text.toString()
+            supplierPhone = fragmentView.findViewById<EditText>(R.id.supplierPhone).text.toString()
+            supplierAddress = fragmentView.findViewById<EditText>(R.id.supplierAddress).text.toString()
+            val insertData = supplierViewModel.insertDataSetToRoomDB(supplierName, supplierEmail, supplierPhone, supplierAddress)
+
+            if(insertData){
+                //If insertData operation is successful navigate back to Supplier Fragment
+                findNavController().navigate(R.id.action_addSupplierFragment_to_supplierFragment)
+            }
+
+        }
+
+        return fragmentView
+
     }
+
+
+
 }
--- app/src/main/java/app/smartmanager/ui/setup/viewmodel/SupplierViewModel.kt
+++ app/src/main/java/app/smartmanager/ui/setup/viewmodel/SupplierViewModel.kt
@@ -1,10 +1,15 @@
 package app.smartmanager.ui.setup.viewmodel
 
 import android.app.Application
+import android.text.TextUtils
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.viewModelScope
 import app.smartmanager.datalayer.entity.Supplier
+import app.smartmanager.helper.GetAppContext
+import app.smartmanager.helper.ToastMaker
 import app.smartmanager.repository.SmartManagerRepo
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -12,6 +17,12 @@ import kotlinx.coroutines.launch
 class SupplierViewModel(application: Application) : AndroidViewModel(application) {
 
     private val readAllSupplierData: LiveData<List<Supplier>>
+
+
+
+
+
+
 
     //Retrieve instance of SmartManager's Repository
     private val repository: SmartManagerRepo = SmartManagerRepo.get()
@@ -25,5 +36,35 @@ class SupplierViewModel(application: Application) : AndroidViewModel(application
         viewModelScope.launch(Dispatchers.IO)  {
             repository.addSupplier(supplier)
         }
+    }
+
+    fun insertDataSetToRoomDB(supplierName: String, supplierEmail: String?, supplierPhone: String?, supplierAddress: String?): Boolean {
+
+        if(checkInputData(supplierName)){
+            //Create a Supplier object
+            val supplier = Supplier(0,supplierName,supplierEmail,supplierPhone,supplierAddress)
+
+            //Calling ViewModel function to add supplier object to Database
+            addSupplier(supplier)
+
+            //Inform UI of successful entry
+            ToastMaker.showToast("Supplier added successfully!", GetAppContext.appContext)
+            return true
+        } else {
+            //In case the input check fails
+            ToastMaker.showToast("Enter supplier name", GetAppContext.appContext)
+            return false
+        }
+
+
+
+
+    }
+
+    /*
+    Function to check if not-null fields are not empty
+     */
+    private fun checkInputData(supplierName: String): Boolean{
+        return !(TextUtils.isEmpty(supplierName))
     }
 }
