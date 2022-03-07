53ea3c650a409c0eca4cd37f77b6090cfe17a3df
Salik Tariq <saliktariq@icloud.com>
2022-03-07 20:43:11 +0000

    data access objects for new classes written

--- app/src/main/java/app/smartmanager/datalayer/dataaccessobjects/CookedProductItemDAO.kt
+++ app/src/main/java/app/smartmanager/datalayer/dataaccessobjects/CookedProductItemDAO.kt
@@ -1,4 +1,21 @@
 package app.smartmanager.datalayer.dataaccessobjects
 
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import app.smartmanager.datalayer.entity.CookedProductItem
+
+@Dao
 interface CookedProductItemDAO {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun addCookedProductItemRecord(record: CookedProductItem)
+
+    @Query("Select * from cooked_product_item ORDER BY id ASC")
+    fun readAllCookedProductItemData(): LiveData<List<CookedProductItem>>
+
+    @Update
+    fun updateCookedProductItemRecord(record: CookedProductItem)
+
+    @Delete
+    fun deleteCookedProductItemRecord(record: CookedProductItem)
+
 }
--- app/src/main/java/app/smartmanager/datalayer/dataaccessobjects/InventoryItemDAO.kt
+++ app/src/main/java/app/smartmanager/datalayer/dataaccessobjects/InventoryItemDAO.kt
@@ -1,4 +1,21 @@
 package app.smartmanager.datalayer.dataaccessobjects
 
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import app.smartmanager.datalayer.entity.InventoryItem
+
+@Dao
 interface InventoryItemDAO {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun addInventoryItemRecord(record: InventoryItem)
+
+    @Query("Select * from cooked_product_item ORDER BY id ASC")
+    fun readAllInventoryItemData(): LiveData<List<InventoryItem>>
+
+    @Update
+    fun updateInventoryItemRecord(record: InventoryItem)
+
+    @Delete
+    fun deleteInventoryItemRecord(record: InventoryItem)
+
 }
