0f1a0bf05f6e6b9ffd83a8792d50ca9599bc6dbe
Salik Tariq <saliktariq@icloud.com>
2022-03-07 23:08:22 +0000

    custom adapters written for 2 new data classes

--- app/src/main/java/app/smartmanager/adapters/CookedProductItemAdapter.kt
+++ app/src/main/java/app/smartmanager/adapters/CookedProductItemAdapter.kt
@@ -1,4 +1,73 @@
 package app.smartmanager.adapters
 
-class CookedProductItemAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.RecyclerView
+import app.smartmanager.R
+import app.smartmanager.datalayer.entity.CookedProductItem
+import app.smartmanager.ui.setup.CookedProductItemFragmentDirections
+import java.text.FieldPosition
+
+class CookedProductItemAdapter :
+RecyclerView.Adapter<CookedProductItemAdapter.MyViewHolder>(){
+    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val cookedProductName = itemView.findViewById<TextView>(R.id.cookedProductName)
+        val quantityPerCookingBatch = itemView.findViewById<TextView>(R.id.quantityPerCookingBatch)
+        val relatedProduct = itemView.findViewById<TextView>(R.id.relatedProduct)
+
+        //Implementing onClickListener to access a single record
+        val cookedProductItemSingleRecord =
+            itemView.findViewById<LinearLayout>(R.id.cookedProductItemSingleRecord)
+
+        fun bind(data: CookedProductItem){
+            cookedProductName.text = data.name
+            quantityPerCookingBatch.text = data.quantityPerCookingBatch.toString()
+            relatedProduct.text = data.relatedProduct
+
+            //Implementing onClickListener to navigate to relevant update fragment
+            cookedProductItemSingleRecord.setOnClickListener {
+                val action = CookedProductItemFragmentDirections.actionCookedProductItemFragmentToUpdateCookedProductItemFragment(data)
+                itemView.findNavController().navigate(action)
+
+            }
+
+
+        }
+
+    }
+
+    // List to contain CookingProductItem objects
+
+    private var cookedProductItemRecordList = emptyList<CookedProductItem>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CookedProductItemAdapter.MyViewHolder {
+
+        //Returning CookedProductItem list itemview
+        return MyViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.cooked_product_item_list_item, parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(
+        holder: CookedProductItemAdapter.MyViewHolder, position: Int
+    ){
+        //Setting current record
+        val currentRecord = cookedProductItemRecordList[position]
+        holder.bind(cookedProductItemRecordList[position])
+    }
+
+    fun setDataSet(cookedProductItem: List<CookedProductItem>){
+        this.cookedProductItemRecordList = cookedProductItem
+
+        //Notifying UI of data update
+        notifyDataSetChanged()
+    }
+
+    override fun getItemCount(): Int {
+        return cookedProductItemRecordList.size
+    }
 }
--- app/src/main/java/app/smartmanager/adapters/InventoryItemAdapter.kt
+++ app/src/main/java/app/smartmanager/adapters/InventoryItemAdapter.kt
@@ -1,4 +1,83 @@
 package app.smartmanager.adapters
 
-class InventoryItemAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.RecyclerView
+import app.smartmanager.R
+import app.smartmanager.datalayer.entity.CookedProductItem
+import app.smartmanager.datalayer.entity.InventoryItem
+import app.smartmanager.ui.setup.CookedProductItemFragmentDirections
+import app.smartmanager.ui.setup.InventoryItemFragmentDirections
+
+class InventoryItemAdapter :
+    RecyclerView.Adapter<InventoryItemAdapter.MyViewHolder>() {
+    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val inventoryName = itemView.findViewById<TextView>(R.id.inventoryName)
+        val inventorySupplier = itemView.findViewById<TextView>(R.id.inventorySupplier)
+        val inventoryQuantityPerUnit =
+            itemView.findViewById<TextView>(R.id.inventoryQuantityPerUnit)
+
+        //Implementing onClickListener to access a single record
+        val inventoryItemSingleRecord =
+            itemView.findViewById<LinearLayout>(R.id.inventoryItemSingleRecord)
+
+        fun bind(data: InventoryItem) {
+            inventoryName.text = data.name
+            inventorySupplier.text = data.supplier
+            inventoryQuantityPerUnit.text = data.quantityPerUnit.toString()
+
+            //Implementing onClickListener to navigate to relevant update fragment
+            inventoryItemSingleRecord.setOnClickListener {
+                val action =
+                    InventoryItemFragmentDirections.actionInventoryItemFragmentToUpdateInventoryItemFragment(
+                        data
+                    )
+                itemView.findNavController().navigate(action)
+
+            }
+
+
+        }
+
+    }
+
+    // List to contain InventoryItem objects
+
+    private var inventoryItemRecordList = emptyList<InventoryItem>()
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): InventoryItemAdapter.MyViewHolder {
+
+        //Returning InventoryItem list itemview
+        return MyViewHolder(
+            LayoutInflater.from(parent.context)
+                .inflate(R.layout.inventory_item_list_item, parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(
+        holder: InventoryItemAdapter.MyViewHolder, position: Int
+    ) {
+        //Setting current record
+        val currentRecord = inventoryItemRecordList[position]
+        holder.bind(inventoryItemRecordList[position])
+    }
+
+    fun setDataSet(inventoryItem: List<InventoryItem>) {
+
+        this.inventoryItemRecordList = inventoryItem
+
+        //Notifying UI of data update
+        notifyDataSetChanged()
+    }
+
+    override fun getItemCount(): Int {
+        return inventoryItemRecordList.size
+    }
 }
