7676af18031e3186258f431441a452e3b75c0de5
Salik Tariq <saliktariq@icloud.com>
2022-03-08 00:14:24 +0000

    fragment classes for data classes
    Fragment classes written for 2 new data classes

--- app/src/main/java/app/smartmanager/ui/setup/CookedProductItemFragment.kt
+++ app/src/main/java/app/smartmanager/ui/setup/CookedProductItemFragment.kt
@@ -2,32 +2,64 @@ package app.smartmanager.ui.setup
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.text.Layout
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import app.smartmanager.R
+import app.smartmanager.adapters.CookedProductItemAdapter
+import app.smartmanager.adapters.ProbeCalibrationRecordAdapter
 import app.smartmanager.ui.setup.viewmodel.CookedProductItemViewModel
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class CookedProductItemFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = CookedProductItemFragment()
-    }
-
-    private lateinit var viewModel: CookedProductItemViewModel
+    private lateinit var cookedProductItemViewModel: CookedProductItemViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.cooked_product_item_fragment, container, false)
+        val fragmentView = inflater.inflate(R.layout.cooked_product_item_fragment, container, false)
+        val floatingAddButton = fragmentView.findViewById<FloatingActionButton>(R.id.btnAdd)
+        val backButton = fragmentView.findViewById<Button>(R.id.btnBack)
+
+        //Implementing recyclerview
+        val adapter = CookedProductItemAdapter()
+        val recyclerView = fragmentView.findViewById<RecyclerView>(R.id.displayDataView)
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+
+        //Initialising the view model
+
+        cookedProductItemViewModel = ViewModelProvider(this).get(CookedProductItemViewModel::class.java)
+
+        cookedProductItemViewModel.readAllCookedProductItemRecordData.observe(viewLifecycleOwner, Observer {
+            cookedProductItemData ->
+            adapter.setDataSet(cookedProductItemData)
+        })
+
+        //Attaching onClickListener to Add Button (floating add button)
+        floatingAddButton.setOnClickListener {
+            findNavController().navigate(R.id.action_cookedProductItemFragment_to_addCookedProductItemFragment)
+        }
+
+        //Attaching onClickListener to Back button
+        backButton.setOnClickListener{
+            findNavController().navigate(R.id.action_cookedProductItemFragment_to_initialSettings)
+        }
+
+        return fragmentView
+
+
+
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(CookedProductItemViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
 
 }
--- app/src/main/java/app/smartmanager/ui/setup/InventoryItemFragment.kt
+++ app/src/main/java/app/smartmanager/ui/setup/InventoryItemFragment.kt
@@ -6,28 +6,53 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import app.smartmanager.R
+import app.smartmanager.adapters.InventoryItemAdapter
+import app.smartmanager.adapters.ProbeCalibrationRecordAdapter
 import app.smartmanager.ui.setup.viewmodel.InventoryItemViewModel
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class InventoryItemFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = InventoryItemFragment()
-    }
-
-    private lateinit var viewModel: InventoryItemViewModel
-
+    private lateinit var inventoryItemViewModel: InventoryItemViewModel
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.inventory_item_fragment, container, false)
-    }
+        val fragmentView = inflater.inflate(R.layout.inventory_item_fragment, container, false)
+        val floatingAddButton = fragmentView.findViewById<FloatingActionButton>(R.id.btnAdd)
+        val backButton = fragmentView.findViewById<Button>(R.id.btnBack)
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(InventoryItemViewModel::class.java)
-        // TODO: Use the ViewModel
+        //Implementing recyclerview
+        val adapter = InventoryItemAdapter()
+        val recyclerView = fragmentView.findViewById<RecyclerView>(R.id.displayDataView)
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+
+        //Initialising the viewmodel
+        inventoryItemViewModel = ViewModelProvider(this).get(InventoryItemViewModel::class.java)
+
+        inventoryItemViewModel.readAllInventoryItemRecordData.observe(viewLifecycleOwner, Observer {
+            inventoryItemData ->
+            adapter.setDataSet(inventoryItemData)
+        })
+
+        //Attaching onClickListener to Add button (btnAdd)
+        floatingAddButton.setOnClickListener {
+            findNavController().navigate(R.id.action_inventoryItemFragment_to_addInventoryItemFragment)
+        }
+
+        //Attaching onClickListener to BackButton (btnBack)
+        backButton.setOnClickListener {
+            findNavController().navigate(R.id.action_inventoryItemFragment_to_initialSettings)
+        }
+        return fragmentView
     }
 
 }
