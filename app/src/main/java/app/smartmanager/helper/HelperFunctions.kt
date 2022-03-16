64d309b9d0739d1eb08853392be11a2f2833b095
Salik Tariq <saliktariq@icloud.com>
2022-03-16 23:44:37 +0200

    updated auxiliary functions

--- app/src/main/java/app/smartmanager/helper/HelperFunctions.kt
+++ app/src/main/java/app/smartmanager/helper/HelperFunctions.kt
@@ -46,6 +46,40 @@ Function to check if not-null fields are not empty
 
             return calendar.time
         }
+        /*
+        generateAuthCode is my own work that I wrote while completing a module project at Birkbeck
+         */
+        fun generateAuthCode(
+            username: String?,
+            pwd: String,
+            email: String,
+            firstName: String?
+        ): Long {
+
+            val userHash = username.hashCode()
+            val pwdHash = pwd.hashCode()
+            val emailHash = email.hashCode()
+            val firstNameHash = firstName.hashCode()
+            val currentTime =
+                System.currentTimeMillis() //Adding a random unique variable to the hash mix
+
+            var hashCode = userHash + pwdHash + emailHash + firstNameHash + currentTime
+            val stringLength = hashCode.toString().length
+            val targetLength: Int
+
+            //Managing the length of hashCode
+            //Update this variable to control the length of auth code.
+            val codeLength = 13
+            targetLength = if (stringLength < codeLength) {
+                stringLength
+            } else {
+                codeLength
+            }
+
+            //Setting the desired length of the hashCode, and returning it as a Long
+            hashCode = (hashCode.toString().substring(0, targetLength)).toLong()
+            return hashCode
+        }
     }
 
 }
---
+++ app/src/main/java/app/smartmanager/helper/SMTPAccount.kt
@@ -0,0 +1,8 @@
+package app.smartmanager.helper
+
+class SMTPAccount {
+    companion object{
+        const val USER: String = "youremail@emailaddress.com"
+        const val PASSWORD: String = "yourSMTPpassword"
+    }
+}
