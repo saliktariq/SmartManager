de4beaa0a2224b8448782ede66dd0f884b4c78e2
Salik Tariq <saliktariq@icloud.com>
2022-03-21 05:42:42 +0200

    Create DatabaseTest.kt

---
+++ app/src/androidTest/java/app/smartmanager/DatabaseTest.kt
@@ -0,0 +1,46 @@
+package app.smartmanager
+
+import app.smartmanager.datalayer.database.SmartManagerDB
+import org.junit.Assert.assertEquals
+import androidx.room.Room
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.After
+import org.junit.Assert.assertNotNull
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.io.IOException
+
+
+
+@RunWith(AndroidJUnit4::class)
+class DatabaseTest{
+    private lateinit var database: SmartManagerDB
+    @Test
+    @Throws(Exception::class)
+
+
+
+    @Before
+    fun createDb(){
+        val context = InstrumentationRegistry.getInstrumentation().targetContext
+        database = Room.inMemoryDatabaseBuilder(context, SmartManagerDB::class.java)
+            .allowMainThreadQueries()
+            .build()
+
+
+    }
+
+    @After
+    @Throws(IOException::class)
+    fun closeDb() {
+        database.close()
+    }
+
+
+    @Test
+    fun databaseExistsTest() {
+        assertNotNull(database)
+    }
+}
